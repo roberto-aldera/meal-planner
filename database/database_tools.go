3347e9ce44158d2f7bd2658123e4e97d2b9cb12b
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2021-06-01 09:20:00 +0100

    Successfully pull in meal data from database

--- database/database_tools.go
+++ database/database_tools.go
@@ -1,9 +1,32 @@
 package database
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 )
 
 func RunMe() {
 	fmt.Println("Hello from database_tools!")
+
+	// open database
+	sqliteDatabase, _ := sql.Open("sqlite3", "/Users/roberto/github-code/meal-planner/localdata/meal-data.db")
+	defer sqliteDatabase.Close()
+
+	displayEntries(sqliteDatabase)
+	fmt.Println("All done!")
+}
+
+func displayEntries(db *sql.DB) {
+	row, err := db.Query("SELECT Meal, Hours FROM meals ORDER BY Hours") // todo: using * for all wasn't working
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() {
+		var Meal string
+		var time string
+		row.Scan(&Meal, &time)
+		log.Println("Meal: ", Meal, " ", time)
+	}
 }
