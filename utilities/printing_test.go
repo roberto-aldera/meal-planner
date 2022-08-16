60285b878d0e31e334459c36b756996daec218f3
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2022-08-16 08:47:58 +0100

    Add tests for PrintExcludedMeals

--- utilities/printing_test.go
+++ utilities/printing_test.go
@@ -52,3 +52,39 @@ func TestPrintMealDatabaseWithCategories(t *testing.T) {
 		t.Fatal(err.Error())
 	}
 }
+
+func TestPrintExcludedMeals(t *testing.T) {
+	// First just check with empty database
+	var emptyMealMap map[int]database.Meal
+	previousMealsToExclude := []int{123}
+	err := PrintExcludedMeals(emptyMealMap, previousMealsToExclude)
+	if err == nil {
+		t.Fatal("Expected an error when using an empty meal map.")
+	}
+
+	sqliteDatabase, _ := sql.Open("sqlite3", "../meal-data.db")
+	defer sqliteDatabase.Close()
+	allMealsFromDatabase := database.LoadDatabaseEntriesIntoContainer(sqliteDatabase)
+	mealMap, _ := MakeMealMap(allMealsFromDatabase)
+
+	// Check if no meals were excluded
+	previousMealsToExclude = []int{}
+	err = PrintExcludedMeals(mealMap, previousMealsToExclude)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	// Check if excluded meal doesn't exist
+	previousMealsToExclude = []int{1}
+	err = PrintExcludedMeals(mealMap, previousMealsToExclude)
+	if err == nil {
+		t.Fatal("Expected an error when using a non-existent ID.")
+	}
+
+	// An finally check happy path runs as expected
+	previousMealsToExclude = []int{123}
+	err = PrintExcludedMeals(mealMap, previousMealsToExclude)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
