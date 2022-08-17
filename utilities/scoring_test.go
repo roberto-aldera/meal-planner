74925f732f02bd08d1204d5624691414678e7d51
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2022-08-17 09:02:21 +0100

    Add another test case for scoring function

--- utilities/scoring_test.go
+++ utilities/scoring_test.go
@@ -9,8 +9,8 @@ import (
 func TestCalculateScore(t *testing.T) {
 	filePath := "../default_config.json"
 	config, _ := LoadConfiguration(filePath)
-	config.PreferenceMealDaysOfWeek = []int{3}
-	config.PreferenceMealIDs = []int{755}
+	// config.PreferenceMealDaysOfWeek = []int{3}
+	// config.PreferenceMealIDs = []int{755}
 
 	var emptyMeal database.Meal
 	weekPlan := []database.Meal{emptyMeal, emptyMeal, emptyMeal, emptyMeal, emptyMeal, emptyMeal, emptyMeal}
@@ -20,4 +20,13 @@ func TestCalculateScore(t *testing.T) {
 		t.Fatal("Expected an error when using a week meal plan with at least one empty meal.")
 	}
 
+	var realMeal database.Meal
+	realMeal.ID = 123
+	realMeal.MealName = "A tasty dish"
+	weekPlan = []database.Meal{realMeal, realMeal, realMeal, realMeal, realMeal, realMeal, realMeal}
+
+	_, err = CalculateScore(weekPlan, config)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
 }
